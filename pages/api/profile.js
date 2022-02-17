07c96fca9732bb57a30cb7a55a88eed0bad4cad1
maniraj0601 <iammaniraja06@gmail.com>
2022-02-18 02:18:23 +0530

    Initial Repo Upload
    Upload application files

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import myData from "../../public/myData.json";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(myData.data);
 }
